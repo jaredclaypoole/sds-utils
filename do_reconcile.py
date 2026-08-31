d16dcb2b6bcdf621c3aba7ef16b43a6bdc80b0e7
Jared Claypoole <jc1617@princeton.edu>
2026-08-31 07:17:01 -0400

    [no-verify] [reconcile] Fully reconcile Aug 1-29

--- do_reconcile.py
+++ do_reconcile.py
@@ -27,8 +27,6 @@ with app.setup:
     days_index_cols = "Instrument,Level,Descriptor,Date".split(",")
     repoints_index_cols = "Instrument,Level,Descriptor,Repoint".split(",")
 
-    repoint0_date: datetime.date = datetime.date(2025, 9, 8)
-
 
 @app.cell(hide_code=True)
 def _():
@@ -42,7 +40,8 @@ def _():
 def _():
     # scrubber_base_dir = Path("output/july-august-only/no-combine-missing")
     # scrubber_base_dir = Path("output/scrubber/full/run_2026-08-26/no-combine-missing")
-    scrubber_base_dir = Path("output/scrubber/full/run_2026-08-28_15-30")
+    # scrubber_base_dir = Path("output/scrubber/full/run_2026-08-28_15-30")
+    scrubber_base_dir = Path("output/scrubber/full/run_2026-08-31_06-35")
     fpath_missing_days = scrubber_base_dir / "missing_days.csv"
     fpath_missing_repoints = scrubber_base_dir / "missing_repoints.csv"
     fpath_all_products = scrubber_base_dir / "all_products.csv"
@@ -54,7 +53,10 @@ def _():
     #     dashboard_base_dir / "imap-run-status-all-rows-20260828T115625Z.csv"
     # )
     fpath_dashboard_output = Path(
-        "dashboard-output/august-only/aug_1-14/run_2026-08-28_15-10/imap-run-status-all-rows-20260828T192006Z.csv"
+        # "dashboard-output/august-only/aug_1-14/run_2026-08-28_15-10/imap-run-status-all-rows-20260828T192006Z.csv"
+        # "dashboard-output/august-only/aug_1-29/run_2026-08-31_06-28/imap-run-status-all-rows-20260831T102809Z.csv"
+        # "dashboard-output/august-only/aug_1-29/run_2026-08-31_06-30/imap-run-status-all-rows-20260831T105056Z.csv"
+        "dashboard-output/august-only/aug_1-29/run_2026-08-31_06-57/imap-run-status-all-rows-20260831T105726Z.csv"
     )
     dashboard_base_dir = fpath_dashboard_output.parent
 
@@ -64,9 +66,11 @@ def _():
     # dashboard_end_repoint = 325
 
     dashboard_start_date = pd.to_datetime(datetime.date(2026, 8, 1))
-    dashboard_end_date = pd.to_datetime(datetime.date(2026, 8, 14))
     dashboard_start_repoint = 327
-    dashboard_end_repoint = 340
+    # dashboard_end_date = pd.to_datetime(datetime.date(2026, 8, 14))
+    # dashboard_end_repoint = 340
+    dashboard_end_date = pd.to_datetime(datetime.date(2026, 8, 29))
+    dashboard_end_repoint = 355
     return (
         dashboard_end_date,
         dashboard_end_repoint,
@@ -338,7 +342,7 @@ def _():
     ) -> Sequence[tuple[pd.Timestamp, pd.Timestamp] | pd.Timestamp]:
         return value_ranges(dates, collapse=collapse, unit=datetime.timedelta(days=1))  # type: ignore
 
-    return date_ranges, partition_ranges
+    return (date_ranges,)
 
 
 @app.function
@@ -413,6 +417,7 @@ def _(
     # assert that the dashboard misses nothing from the scrubber
     _df = _df[_in_range_mask]
     _df = _df[~_df.from_dashboard]
+    sddf = _df
     assert len(_df) == 0
 
     ddf_markup_full = ddf_markup
@@ -435,11 +440,12 @@ def _(
     # ddf_markup
 
     # len(dashboard_df), len(ddf_markup)
-    return (ddf_markup,)
+    return ddf_markup, sddf
 
 
 @app.cell
-def _():
+def _(sddf):
+    sddf
     return
 
 
@@ -481,7 +487,7 @@ def _(dashboard_start_date, mddf):
         return False
 
     mddf_after = mddf[~mddf.date_ranges_full.map(_f_mask)]
-    mddf_after
+    mddf_after;
     return (mddf_after,)
 
 
@@ -506,7 +512,7 @@ def _(mddf):
 
 @app.cell
 def _(mddf_after):
-    group_by_date_ranges(mddf_after)
+    group_by_date_ranges(mddf_after);
     return
 
 
@@ -530,340 +536,10 @@ def _(ddf_markup):
 
 
 @app.cell
-def _(ddf_markup):
-    _df = ddf_markup
+def _(mddf):
+    _df = mddf
+    _df = _df[_df.Instrument == "swapi"]
     _df
-    return
-
-
-@app.cell(hide_code=True)
-def _():
-    mo.md(r"""
-    ## Old
-    """)
-    return
-
-
-@app.cell
-def _(ddf_markup):
-    ddf_markup.scrubber_out_of_range.value_counts()
-    return
-
-
-@app.cell
-def _(ddf_markup):
-    _mddf = ddf_markup
-    len(_mddf), _mddf.product_in_scrubber.sum(), _mddf.scrubber_out_of_range.sum()
-    return
-
-
-@app.cell
-def _(all_products_df):
-
-    _apdf = all_products_df
-    _apdf
-    return
-
-
-@app.cell
-def _(dashboard_df):
-    ddf = dashboard_df
-    ddf = ddf[ddf["Status"] != "materialized"]
-    daily_instruments = set(DAILY_INSTRUMENTS)
-    _df = ddf["Instrument,Level,Descriptor,Date,Repoint".split(",")]
-    _df = _df[_df.Instrument.map(lambda instr: instr in daily_instruments)]
-    _df = _df[_df.Repoint.isna()]
-    _df = _df.drop(columns=["Repoint"])
-    mddf_days = _df
-    _df = ddf["Instrument,Level,Descriptor,Repoint".split(",")]
-    _df = _df[~_df.Repoint.isna()]
-    mddf_repoints = _df
-    return ddf, mddf_days, mddf_repoints
-
-
-@app.cell
-def _(ddf):
-    ddf
-    return
-
-
-@app.cell
-def _(mddf_days, missing_days_df):
-    ddf_1 = mddf_days
-    _df = missing_days_df
-    _jdf = (
-        ddf_1.set_index(days_index_cols)
-        .assign(from_dashboard=True)
-        .join(_df.set_index(days_index_cols).assign(from_scrubber=True), how="outer")
-    )
-    _jdf = _jdf.fillna(False)
-    joined_df_days = _jdf
-    _jdf
-    return (joined_df_days,)
-
-
-@app.cell
-def _(joined_df_days):
-    _df = joined_df_days
-    _df = _df.reset_index()
-    _df = _df[_df.Date >= pd.Timestamp("2026-07-01")]
-    _df = _df[_df.Date < pd.Timestamp("2026-08-01")]
-    _df = _df[_df.from_dashboard != _df.from_scrubber]
-    print(f"len(df)={len(_df)!r}")
-    days_index_not_in_scrubber = _df.set_index(days_index_cols).index
-
-    assert _df.from_scrubber.sum() == 0
-    return (days_index_not_in_scrubber,)
-
-
-@app.cell
-def _(missing_days_df):
-    _df = missing_days_df
-    _df = _df[_df.Date >= pd.Timestamp("2026-07-01")]
-    _df = _df[_df.Date < pd.Timestamp("2026-08-01")]
-    print(f"len(df)={len(_df)!r}")
-    _df
-    return
-
-
-@app.cell
-def _(dashboard_df, days_index_not_in_scrubber):
-    ddf_days_not_in_scrubber = (
-        dashboard_df.set_index(days_index_cols)
-        .loc[days_index_not_in_scrubber]
-        .reset_index()
-    )
-    return (ddf_days_not_in_scrubber,)
-
-
-@app.cell(hide_code=True)
-def _():
-    mo.md(r"""
-    * We've shown all missing days indicated by the scrubber (starting 2026-08-01) are also indicated by the dashboard
-    * Now we filter the missing days indicated by the dashboard but not the scrubber
-      * First we aggregate dates into consecutive ranges
-      * Next we drop products that are missing for a single range that includes the end date, as those will be invisible to the scrubber
-      * We're left with a single product that happened to be materialized in the past but had a failed latest run (with updated dependencies)
-        * This would be invisible to the scrubber
-    """)
-    return
-
-
-@app.cell
-def _(date_ranges, ddf_days_not_in_scrubber):
-    ddf_2 = ddf_days_not_in_scrubber
-    start_date = ddf_2.Date.min()
-    end_date = ddf_2.Date.max()
-    full_date_range = [(start_date, end_date)]
-    print((start_date, end_date))
-    _grouped = ddf_2.groupby("Instrument,Level,Descriptor".split(","))
-
-    def _make_df(_df: pd.DataFrame) -> pd.Series:
-        return pd.Series(
-            dict(
-                date_ranges=_df.Date.apply(date_ranges, by_row=False, collapse=False),
-                first_link=_df["Partition link"].iloc[0],
-            ),
-        )
-
-    _df = _grouped.apply(_make_df)
-    _mask = _df.date_ranges.map(lambda r: r != full_date_range)
-    _df = _df[_mask]
-    _mask = _df.date_ranges.map(lambda r: len(r) > 1 or r[0][1] != end_date)
-    _df = _df[_mask]
-    ddf_days_not_in_scrubber_filtered = _df
-    _df
-    return (ddf_days_not_in_scrubber_filtered,)
-
-
-@app.cell
-def _(mddf_repoints, missing_repoints_df):
-    ddf_3 = mddf_repoints
-    _df = missing_repoints_df
-    _jdf = (
-        ddf_3.set_index(repoints_index_cols)
-        .assign(from_dashboard=True)
-        .join(
-            _df.set_index(repoints_index_cols).assign(from_scrubber=True), how="outer"
-        )
-    )
-    _jdf = _jdf.fillna(False)
-    joined_df_repoints = _jdf
-    _jdf
-    return (joined_df_repoints,)
-
-
-@app.cell
-def _(dashboard_df, joined_df_repoints):
-    _df = joined_df_repoints
-    _df = _df.reset_index()
-    _min_repoint = dashboard_df.Repoint.min()
-    _max_repoint = dashboard_df.Repoint.max()
-    _df = _df[_df.Repoint >= _min_repoint]
-    _df = _df[_df.Repoint <= _max_repoint]
-    _df = _df[_df.from_dashboard != _df.from_scrubber]
-    print(f"len(df)={len(_df)!r}")
-    repoints_index_not_in_scrubber = _df.set_index(repoints_index_cols).index
-
-    assert _df.from_scrubber.sum() == 0
-
-    _df
-    return (repoints_index_not_in_scrubber,)
-
-
-@app.cell
-def _(dashboard_df, repoints_index_not_in_scrubber):
-    ddf_repoints_not_in_scrubber = (
-        dashboard_df.set_index(repoints_index_cols)
-        .loc[repoints_index_not_in_scrubber]
-        .reset_index()
-    )
-    return (ddf_repoints_not_in_scrubber,)
-
-
-@app.cell(hide_code=True)
-def _():
-    mo.md(r"""
-    * We've shown all missing repoints indicated by the scrubber (starting with the min repoint reported by the dashboard) are also indicated by the dashboard
-    * Now we filter the missing repoints indicated by the dashboard but not the scrubber
-      * First we aggregate repoints into consecutive ranges
-      * Next we drop products that are missing for a single range that includes the final repoint, as those will be invisible to the scrubber
-      * We're left with just an l0 product, which the scrubber specifically excludes
-    """)
-    return
-
-
-@app.cell
-def _(ddf_repoints_not_in_scrubber, partition_ranges):
-    ddf_4 = ddf_repoints_not_in_scrubber
-    _min_repoint = ddf_4.Repoint.min().item()
-    max_repoint = ddf_4.Repoint.max().item()
-    full_repoint_range = [(_min_repoint, max_repoint)]
-    print(full_repoint_range[0])
-    _grouped = ddf_4.groupby("Instrument,Level,Descriptor".split(","))
-
-    def _make_repoints_df(_df: pd.DataFrame) -> pd.Series:
-        return pd.Series(
-            dict(
-                repoint_ranges=_df.Repoint.apply(
-                    partition_ranges, by_row=False, collapse=False
-                ),
-                first_link=_df["Partition link"].iloc[0],
-            )
-        )
-
-    _df = _grouped.apply(_make_repoints_df)
-    # _mask = _df.repoint_ranges.map(lambda r: r != full_repoint_range)
-    _mask = _df.repoint_ranges.map(lambda r: len(r) > 1 or r[0][-1] != max_repoint)
-    _df = _df[_mask]
-    _df = _df.sort_values("repoint_ranges")
-
-    def f_mask(ranges: list[tuple[int, int]]) -> bool:
-        if len(ranges) > 1:
-            return True
-        return ranges[0][-1] != max_repoint
-
-    _df = _df[_df.repoint_ranges.map(f_mask)]
-
-    _df = _df.reset_index()
-
-    # _levels_to_exclude = set("l1a l1b".split())
-    # _mask = (
-    #     (_df.Instrument == "ultra")
-    #     &
-    #     _df.Level.map(lambda s: s in _levels_to_exclude)
-    #     &
-    #     _df.repoint_ranges.map(lambda rr: rr == [(295, 300)])
-    # )
-    # _df = _df[~_mask]
-
-    _df = _df.sort_values(by="Instrument Level Descriptor".split())
-
-    # with pd.option_context('display.max_colwidth', None):
-    #     display(_df)
-
-    ddf4_grouped = _df
-
-    _df
-    return ddf4_grouped, ddf_4
-
-
-@app.cell(hide_code=True)
-def _():
-    mo.md(r"""
-    * Possible improvements to the scrubber
-      * Get a list of all products rather than only sensing data products available to imap-data-access
-        * This will catch products that have never been materialized
-      * Revise the logic so missing products with no "bookending" presence are caught
-        * This will catch products never materialized before or after a certain date
-    """)
-    return
-
-
-@app.cell
-def _(ddf4_grouped, ddf_4):
-    _dfg = ddf4_grouped
-    _df = ddf_4.copy()
-
-    _dfg = _dfg.set_index(index_cols)
-    _df = _df.set_index(index_cols)
-
-    _dfg
-
-    _dfg.repoint_ranges
-    _df.loc[_dfg.index, "repoint_ranges"] = _dfg.repoint_ranges
-    _df
-    return
-
-
-@app.cell
-def _(ddf_4):
-    _df = ddf_4
-    _assets = list(_df.Asset)
-    _partitions = list(_df.Partition)
-    _do_thread_map = False
-    if _do_thread_map:
-        thread_map(
-            lambda tup: latest_attempt_failed_after_previous_success_db(*tup),
-            list(zip(_assets, _partitions, strict=True)),
-            max_workers=10,
-        )
-    return
-
-
-@app.cell
-def _(ddf_4):
-    _df = ddf_4
-    _df = _df.iloc[-18:]
-    _assets = list(_df.Asset)
-    _partitions = list(_df.Partition)
-    _the_iter = list(zip(_assets, _partitions, strict=True))
-
-    print(len(_the_iter))
-
-    # for _asset, _partition in mo.status.progress_bar(_the_iter):
-    for _asset, _partition in _the_iter:
-        break
-    print(_asset, _partition)
-    _do_fcn_call = False
-    if _do_fcn_call:
-        latest_attempt_failed_after_previous_success_db(_asset, _partition)
-    return
-
-
-@app.cell
-def _(ddf_days_not_in_scrubber_filtered):
-    _df = ddf_days_not_in_scrubber_filtered
-    _df
-    return
-
-
-@app.cell
-def _(ddf_4):
-    _to_write = False
-    _df = ddf_4
-    if _to_write:
-        pd.DataFrame.to_csv(_df, "temp.csv")    # _df
     return
 
 
